7900e3c3fb4818e53bd55af5a620b4a9a34183d3
Anand.Narayan <arnarayan@gmail.com>
2022-01-27 18:21:33 -0700

    Initial Commit

---
+++ List Demo/List_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  List_DemoApp.swift
+//  List Demo
+//
+//  Created by Anand Narayan on 2022-01-27.
+//
+
+import SwiftUI
+
+@main
+struct List_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
